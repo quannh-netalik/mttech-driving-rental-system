71a78d2d56d77fe5ffcdd0baf4a4b9f2999c1a7d
Quan Nguyen <quannh.netalik@gmail.com>
2025-11-06 05:14:25 +0700

    docs: fix desc for swagger

--- apps/api/src/utils/openapi-config.ts
+++ apps/api/src/utils/openapi-config.ts
@@ -1,9 +1,17 @@
 import { DocumentBuilder } from '@nestjs/swagger';
 
+const descriptions = [
+  'The official API for Driving Rental System (By MTTech).\n',
+  '## Authentication\n',
+  'The API supports Bearer Auth type only, please insert your token for making request\n',
+  '## Quick Links\n',
+  '- Github: https://github.com/quannh-netalik/mttech-driving-rental-system',
+];
+
 export const buildOpenApiConfig = (port: number) => {
   const configBuilder = new DocumentBuilder()
     .setTitle('MTTech Driving Rental System')
-    .setDescription('The official API for Driving Rental System (By MTTech).\n## Authentication')
+    .setDescription(descriptions.join('\n'))
     .setContact('Quan Nguyen', '', 'quannh.netalik@gmail.com')
     .setVersion('1.0')
     .addBearerAuth(
