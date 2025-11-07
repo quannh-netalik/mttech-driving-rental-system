5f55ed130a4a6000f4dd062cf375eb4b87b44881
Quan Nguyen <quannh.netalik@gmail.com>
2025-11-08 06:13:55 +0700

    refactor: remove next themes

--- packages/ui/src/components/sonner.tsx
+++ packages/ui/src/components/sonner.tsx
@@ -1,12 +1,12 @@
 'use client';
 
 import { CircleCheckIcon, InfoIcon, Loader2Icon, OctagonXIcon, TriangleAlertIcon } from 'lucide-react';
-import { useTheme } from 'next-themes';
-import { Toaster as Sonner, type ToasterProps } from 'sonner';
+import { Toaster as Sonner, type ToasterProps, toast } from 'sonner';
+
+import { useTheme } from '@workspace/ui/providers/theme-provider';
 
 const Toaster = ({ ...props }: ToasterProps) => {
   const { theme = 'system' } = useTheme();
-
   return (
     <Sonner
       theme={theme as ToasterProps['theme']}
@@ -31,4 +31,4 @@ const Toaster = ({ ...props }: ToasterProps) => {
   );
 };
 
-export { Toaster };
+export { toast, Toaster };
