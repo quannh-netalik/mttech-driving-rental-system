8e148483707c51d017c21da23d5a296ec696ee4c
Quan Nguyen <quannh.netalik@gmail.com>
2025-11-08 07:35:08 +0700

    refactor: deprecate admin app (next)

--- apps/admin/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  transpilePackages: ['@workspace/ui'],
-  reactCompiler: true,
-  cacheComponents: true, // For PPR
-};
-
-export default nextConfig;
