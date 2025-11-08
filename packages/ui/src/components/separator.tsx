58dec6bd6f0a09e0dcd321692cdd7cd9ae584934
Quan Nguyen <quannh.netalik@gmail.com>
2025-11-08 15:58:57 +0700

    style(separator): fix styles

--- packages/ui/src/components/separator.tsx
+++ packages/ui/src/components/separator.tsx
@@ -1,10 +1,20 @@
 'use client';
 
-import * as React from 'react';
 import * as SeparatorPrimitive from '@radix-ui/react-separator';
+import * as React from 'react';
 
 import { cn } from '@workspace/ui/lib/utils';
 
+/**
+ * A visual separator component that divides content either horizontally or vertically.
+ * Built on Radix UI Separator primitive with dark mode support.
+ *
+ * @example
+ * ```tsx
+ * <Separator orientation="horizontal" />
+ * <Separator orientation="vertical" className="h-20" />
+ * ```
+ */
 function Separator({
   className,
   orientation = 'horizontal',
@@ -17,7 +27,7 @@ function Separator({
       decorative={decorative}
       orientation={orientation}
       className={cn(
-        'shrink-0 border-1.5 bg-black/20 dark:bg-white/20 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px',
+        'shrink-0 bg-black/20 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px dark:bg-white/20',
         className,
       )}
       {...props}
