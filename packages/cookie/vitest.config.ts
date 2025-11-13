7c6ba423e67b6e94b7b19c181756d7376f643934
Quan Nguyen <quannh.netalik@gmail.com>
2025-11-13 13:08:07 +0700

    feat: refactor cookie package

--- packages/cookie/vitest.config.ts
+++ packages/cookie/vitest.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
 	test: {
 		globals: true,
 		environment: 'jsdom',
-		setupFiles: ['./tests/setup.ts'],
+		setupFiles: ['./vitest.setup.ts'],
 		coverage: {
 			provider: 'v8',
 			reporter: ['text', 'json', 'html'],
