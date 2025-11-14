e97431c85c71e7abdbce9567b9856c6bb282102d
Quan Nguyen <quannh.netalik@gmail.com>
2025-11-14 16:01:28 +0700

    fix: build schema error

--- packages/schema/tsdown.config.ts
+++ packages/schema/tsdown.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsdown';
 
 export default defineConfig({
-	entry: ['./src/*.ts'],
+	entry: ['./src/**/*.ts'],
 	format: ['cjs', 'esm'],
 	platform: 'node',
 	dts: true,
